0c1cf0fc47b22311c6e30bdf16043c7d0c5051fb
Vedha Mahadevan <159766539+Vedha17Mahadevan@users.noreply.github.com>
2025-04-29 13:28:05 +0530

    Delete media/script2.js

--- media/script2.js
+++
@@ -1,8 +0,0 @@
-rogressSteps();
-    initializeNumberInputs();
-    createParticlesBackground();
-    
-    // Initialize option change listeners
-    document.querySelectorAll('select, input').forEach(element => {
-        element.addEventListener('change', updateSummary);
-    });
